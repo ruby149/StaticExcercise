4a0274cf340ffcfcacb0378840dd4fea065c46d1
ruby149 <rubiselbenitez30@gmail.com>
2020-07-02 14:26:46 -0400

    final

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"After conversion;");
+            Console.WriteLine($"celcius {celcius}");
+           
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+           var result = (Fahrenheit - 32) / 1.8;
+           
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            var result = (celsius * 9) / 5 + 32;
+            return result;
+            
+
+        }
+    }
+}   
